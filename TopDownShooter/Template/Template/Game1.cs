642030157aa7b5463fb23bfbcce2e780671eebfa
SweJon <46954315+SweJon@users.noreply.github.com>
2019-12-03 18:33:07 +0100

    Bilder + spelare
    Byter spelarbild till en png bild. La till bilder för fiender osv.

---
+++ TopDownShooter/Template/Template/Game1.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Template
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        World world;
+
+        Basic2d cursor;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            Globals.content = this.Content;
+            Globals.spriteBatch = new SpriteBatch(GraphicsDevice);
+
+
+            cursor = new Basic2d("2d\\Misc\\CursorArrow", new Vector2(0, 0), new Vector2(28, 28));
+
+            Globals.keyboard = new McKeyboard();
+            Globals.mouse = new McMouseControl();
+
+            world = new World();
+        }
+
+        protected override void UnloadContent()
+        {
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            Globals.keyboard.Update();
+            Globals.mouse.Update();
+
+
+            world.Update();
+
+
+            Globals.keyboard.UpdateOld();
+            Globals.mouse.UpdateOld();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            Globals.spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend);
+
+            world.Draw(Vector2.Zero);
+
+            cursor.Draw(new Vector2(Globals.mouse.newMousePos.X, Globals.mouse.newMousePos.Y), new Vector2(0, 0));
+            Globals.spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+#endif
+}
---
+++ TopDownShooter/Template/Template/Source/GamePlay/World.cs
@@ -0,0 +1,38 @@
+﻿#region Includes
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+#endregion
+
+namespace Template
+{
+    public class World
+    {
+        public Hero player;
+
+        public World()
+        {
+            player = new Hero("2d\\player", new Vector2(300, 440), new Vector2(60, 100));
+        }
+
+        public virtual void Update()
+        {
+            player.Update();
+        }
+
+        public virtual void Draw(Vector2 OFFSET)
+        {
+            player.Draw(OFFSET);
+        }
+    }
+}
